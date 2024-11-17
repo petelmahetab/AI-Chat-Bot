62302df72563a9582be181311a57562c4b560a17
Mahe2patel <patelmahetab9020@gmail.com>
2024-11-17 20:49:56 +0530

    Js For building Project

---
+++ Js.js
@@ -0,0 +1,10 @@
+// Get the ELEMENTs
+
+let prompt=document.querySelector('#input');
+
+let Submit=document.querySelector('#circle');
+prompt.addEventListener('keydown',(e)=>{
+    console.log("hii",e)
+})
+
+// console.log(prompt.target.value)
